8b69404e13cf62c7670024fb289d24995f82bf0a
KauaDiniz6287 <kaua.diniz@alunos.ifsuldeminas.edu.br>
2023-04-26 10:40:01 -0300

    aula14

--- aula13/form_usuario.php
+++ aula13/form_usuario.php
@@ -9,13 +9,14 @@
 <body>
     <h1>Cadastro de usuário</h1>
     <hr>
-    <form action="" method="post">
+    <form action="usuario/inserir.php" method="post">
         <label>Nome</label><br>
         <input type="text" name="nome"><br>
         <label>Login</label><br>
         <input type="text" name="login"><br>
-        <label>senha</label><br>
+        <label>Senha</label><br>
         <input type="password" name="senha"><br>
+        <button type="submit">Cadastrar</button>
     </form>
 </body>
 </html>
